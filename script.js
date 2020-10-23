f6f9038b1e7342e3fc804c012daa67a886c62bf4
Browbeans <oliverjohansson3@gmail.com>
2020-10-23 14:05:12 +0200

    Add script for interactivity

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 function clickToChange () {
     var h1 = document.getElementById("clickH1")
-    h1.innerText = (newTitle)
+    h1.innerText = (random, newTitles[random])
 }
 
 
-let newTitle = ["Title", "Hejsan", "Kalas"]
+const newTitles = ["HEj" , "Tjabba", "Flezx"];
 
-function randomTitle(newTitle) {
-    for (var i=0; i < newTitle.length; i++) {
-      result += newTitle(Math.floor(Math.random))  
-    }    
+const random = Math.floor(Math.random() * newTitles.length); 
+
+
+
+function printRandomNumber() {
+    const randomNumber = getRandomNumber();
+    addNumberToPage(randomNumber)
 }
+
+function getRandomNumber () {
+    const randomNumber = Math.random() * 10
+    return Math.round(randomNumber)
+}
+
+function addNumberToPage(number) {
+    const div = document.getElementById("result")
+    div.append(number + " , " )
+}
+
+
+// function random() {
+//     const newTitle = ["Title", "Hejsan", "Kalas"]
+//     const randomTitle = Math.floor(Math.random() * newTitle.length)
+//     return (randomTitle)
+// }
+
+
+
+// const months = ["January", "February", "March", "April", "May", "June", "July"];
+
+// const random = Math.floor(Math.random() * months.length);
+// console.log(random, months[random]);
+
