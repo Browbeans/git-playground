feed652a8dab5ac01fbfec4c9771288ecf2a91c6
Browbeans <oliverjohansson3@gmail.com>
2020-10-23 12:52:54 +0200

    JS changes

---
+++ script.js
@@ -0,0 +1,13 @@
+function clickToChange () {
+    var h1 = document.getElementById("clickH1")
+    h1.innerText = (newTitle)
+}
+
+
+let newTitle = ["Title", "Hejsan", "Kalas"]
+
+function randomTitle(newTitle) {
+    for (var i=0; i < newTitle.length; i++) {
+      result += newTitle(Math.floor(Math.random))  
+    }    
+}
